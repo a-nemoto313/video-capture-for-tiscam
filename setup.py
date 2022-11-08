43fbd4565bb90f05a9c7d8cddbeb93236f7357dd
m-ito348 <m-ito@saitoh.ecnet.jp>
2022-11-08 17:45:22 +0900

    data_filesに指定するパスを ("配布先のディレクトリの相対パス"、["配布したいデータファイルのsetup.pyからの相対パス"])に修正。

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="read camera",
     url="https://github.com/a-nemoto313/video-capture-for-tiscam",
     packages=["my_video_capture"],
-    data_files=[("./my_video_capture", ["./my_video_capture/TIS_UDSHL11_x64.dll"]),
-                ("./my_video_capture", ["./my_video_capture/tisgrabber_x64.dll"])],
+    data_files=[("./Lib/site-packages/my_video_capture", ["./my_video_capture/TIS_UDSHL11_x64.dll"]),
+                ("./Lib/site-packages/my_video_capture", ["./my_video_capture/tisgrabber_x64.dll"])],
     include_package_data=True,
 )
